2d0b8318c4b06c98933e77cf424a034d1ca7f602
Дамба Манджиев <mdamba2020@yandex.ru>
2024-02-28 19:50:50 +0300

    use camel_case into Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ public class Main {
         int balance = 100;// Объявляете переменные для входных данных и
         int value = 1100;// параметров программы: начального счёта,
         int score = 0;// суммы пополнения и тп
-        int bonusbalance;
+        int bonusBalance;
 
         if (value > 1000) {
             score = value / 100;
             balance = balance + value;
-            bonusbalance = balance + score;
-            System.out.print("Баланс пополнен на " + value + " рублей." + " Бонусный счет пополнен на " + score + " баллa/ов. Текущий баланс " + bonusbalance + " рублей.");
+            bonusBalance = balance + score;
+            System.out.print("Баланс пополнен на " + value + " рублей." + " Бонусный счет пополнен на " + score + " баллa/ов. Текущий баланс " + bonusBalance + " рублей.");
 
         } else {
             balance = balance + value;
-            bonusbalance = balance + score;
-            System.out.print("Баланс пополнен на " + value + " рублей." + " Бонусный счет пополнен на " + score + " баллa/ов. Текущий баланс " + bonusbalance + " рублей.");
+            bonusBalance = balance + score;
+            System.out.print("Баланс пополнен на " + value + " рублей." + " Бонусный счет пополнен на " + score + " баллa/ов. Текущий баланс " + bonusBalance + " рублей.");
 
 
         }
